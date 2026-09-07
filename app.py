f13b640deee46a8ebba4bc1bc21ffcc47ba402f9
CKrsevan <160563903+CKrsevan@users.noreply.github.com>
2026-09-07 23:36:50 +0000

    compact3

--- app.py
+++ app.py
@@ -1249,8 +1249,8 @@ with tab2:
     """
     }
 
-    cols = st.columns(4, gap="small")
-    
+    cols = st.columns(4, gap="medium")
+
     for i, (permit_name, email_text) in enumerate(emails.items()):
 
         with cols[i % 3]:
