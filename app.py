c8cd9b284304ce6702e4fe88aab7a5e654c1b1fa
CKrsevan <160563903+CKrsevan@users.noreply.github.com>
2026-09-08 00:16:28 +0000

    biggerbuttons10

--- app.py
+++ app.py
@@ -1273,7 +1273,7 @@ for i, (permit_name, email_text) in enumerate(emails.items()):
                 unsafe_allow_html=True
             )
 
-            left, middle, right = st.columns([0.1, 0.8, 0.1])
+            left, middle, right = st.columns([0.05, 0.9, 0.05])
 
             with middle:
                 st_copy_to_clipboard(
